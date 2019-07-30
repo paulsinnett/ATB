989ff84c47addeb9da63a06048a7af94bfef66bd
Paul Sinnett <paul.sinnett@gmail.com>
2019-07-30 23:09:23 +0100

    Initial commit

---
+++ Assets/Creature.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour
+{
+	public int speed = 10;
+	float initiative = 0.0f;
+
+	void Update()
+	{
+		initiative += speed * Time.deltaTime;
+	}
+}
